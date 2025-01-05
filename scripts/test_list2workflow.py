441d912918a9181d72e32b397597cec78bb5a927
Jacob Wujciak-Jens <jacob@wujciak.de>
2025-01-05 02:59:19 +0100

    consolidate scripts

--- scripts/test_list2workflow.py
+++
@@ -1,7 +0,0 @@
-import list2workflow as l2
-def test_all():
-    l = l2.load_list("list.yaml")
-    wf = l2.generate_workflow(l)
-
-    with open(".github/workflows/dummy.yml", "w") as file:
-        file.write(wf)
